2686c9731652b6b26962ba1e172a155071b289c4
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2023-09-18 13:29:05 +0200

    daw2

---
+++ Ejemplo1/JS/script.js
@@ -0,0 +1,52 @@
+/*let age;
+age  = prompt("dime tu edad")
+console.log(age);
+
+
+let result;
+result = confirm("Estas seguro que quieres continuar ?")
+console.log(result);
+
+if (result==true) {
+    age=34
+} else {
+    age=12
+}*/
+
+/*let num;
+
+num=prompt("escriu un numero: ");
+console.log(num);
+if(isNaN(num)){
+  alert( "No es un numero");
+
+}else{ 
+  //fem el producte
+  let prod=1;
+  for(let i=1; i<=num;i++){
+    prod*=i;
+
+  }
+console.log(prod);
+}*/
+// showMessage()
+// function showMessage(){
+
+//     let message= "hello"
+//     alert(message)
+    
+//     }
+    
+
+    //Funion arrow
+let salutacion = function () {
+    return "klk majinbu "
+}
+alert(salutacion())
+
+let salutaciones = (message)=> {
+
+    return "klk majinbu 2" + message
+
+}
+alert(salutaciones("saludos manin"))
