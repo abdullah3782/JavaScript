eafff6c69697f8973c66647d99a13166afabe91c
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2023-09-20 11:25:24 +0200

    js

---
+++ Ejercicios/Ex2/JS/index.js
@@ -0,0 +1,32 @@
+function sumar(num1, num2) {
+
+    num1 = Number(prompt("Ingrese el primer número"));
+    num2 = Number(prompt("Ingrese el segundo número"));
+    resultat = (num1 + num2);
+    alert(resultat)
+}
+
+function restar(num1, num2) {
+    num1 = prompt(("Ingrese el primer número"))
+    num2 = prompt(("Ingrese el segundo número"))
+    resultat = num1 - num2;
+    alert(resultat)
+}
+
+function multiplicar(num1, num2) {
+    num1 = prompt(("Ingrese el primer número"))
+    num2 = prompt(("Ingrese el segundo número"))
+    resultat = num1 * num2;
+    alert(resultat)
+}
+
+function dividir(num1, num2) {
+    num1 = prompt(("Ingrese el primer número"))
+    num2 = prompt(("Ingrese el segundo número"))
+    resultat = num1 / num2;
+    alert(resultat)
+}
+sumar()
+restar()
+multiplicar()
+dividir()
