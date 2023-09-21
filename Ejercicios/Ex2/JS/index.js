4c35141fb123f7cd98c1203bc40d5316cded3694
abdullah3782 <93907645+abdullah3782@users.noreply.github.com>
2023-09-21 08:52:33 +0200

    js

--- Ejercicios/Ex2/JS/index.js
+++ Ejercicios/Ex2/JS/index.js
@@ -1,32 +1,36 @@
-function sumar(num1, num2) {
+// function sumar(num1, num2) {
 
-    num1 = Number(prompt("Ingrese el primer número"));
-    num2 = Number(prompt("Ingrese el segundo número"));
-    resultat = (num1 + num2);
-    alert(resultat)
-}
+//     num1 = Number(prompt("Ingrese el primer número per sumar"));
+//     num2 = Number(prompt("Ingrese el segundo número per sumar"));
+//     resultat = (num1 + num2);
+//     alert(resultat)
+// }
 
-function restar(num1, num2) {
-    num1 = prompt(("Ingrese el primer número"))
-    num2 = prompt(("Ingrese el segundo número"))
-    resultat = num1 - num2;
-    alert(resultat)
-}
+// function restar(num1, num2) {
+//     num1 = prompt(("Ingrese el primer número per restar"))
+//     num2 = prompt(("Ingrese el primer número per restar"))
+//     resultat = num1 * num2;
+//     alert(resultat)
+// }
 
-function multiplicar(num1, num2) {
-    num1 = prompt(("Ingrese el primer número"))
-    num2 = prompt(("Ingrese el segundo número"))
-    resultat = num1 * num2;
-    alert(resultat)
-}
+// function multiplicar(num1, num2) {
+//     num1 = prompt(("Ingrese el primer número per multiplicar"))
+//     num2 = prompt(("Ingrese el primer número per multiplicar"))
+//     resultat = num1 * num2;
+//     alert(resultat)
+// }
 
-function dividir(num1, num2) {
-    num1 = prompt(("Ingrese el primer número"))
-    num2 = prompt(("Ingrese el segundo número"))
-    resultat = num1 / num2;
-    alert(resultat)
+// function dividir(num1, num2) {
+//     num1 = prompt(("Ingrese el primer número per dividir"))
+//     num2 = prompt(("Ingrese el primer número per dividir"))
+//     resultat = num1 * num2;
+//     alert(resultat)
+// }
+// sumar()
+
+//ARROW FUNCTIONS
+let suma=()=> {
+
+
+    
 }
-sumar()
-restar()
-multiplicar()
-dividir()
