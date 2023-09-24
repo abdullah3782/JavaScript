35381a650f50f43a1ee03c1729c2dbbeb9116a93
Abdullah Waris <abdullah.waris098@gmail.com>
2023-09-24 13:10:39 +0200

    deberes2

--- Vueling/JS/script.js
+++ Vueling/JS/script.js
@@ -10,6 +10,13 @@ const cfvForm = document.getElementById("cfvForm");
 
 const tpButton = document.getElementById("cfv");
 const tpForm = document.getElementById("tpForm");
+
+const nadaButton = document.getElementById("nada");
+
+const panelViajeButton = document.getElementById("panelviaje");
+const panelViaje2Button = document.getElementById("panelviaje2");
+
+
 // Agrega eventos clic a los botones para mostrar/ocultar los formularios.
 // Agrega eventos clic a los botones para mostrar/ocultar los formularios y elementos adicionales.
 loginButton.addEventListener("click", function() {
@@ -17,14 +24,20 @@ loginButton.addEventListener("click", function() {
   registerForm.style.display = "none";
   cfv.style.display = "block";
   cfvForm.style.display = "none";
-  tp.style.display = "block";
+  tpForm.style.display = "none";
+  panelViajeButton.style.display = "none";
+  panelViaje2Button.style.display = "none";
+
+
 });
 
 registerButton.addEventListener("click", function() {
   loginForm.style.display = "none";
   registerForm.style.display = "block";
   cfvForm.style.display = "none";
-  tp.style.display = "none";
+  tpForm.style.display = "none";
+  panelViajeButton.style.display = "none";
+  panelViaje2Button.style.display = "none";
 });
 
 cfvButton.addEventListener("click", function() {
@@ -40,3 +53,13 @@ tpButton.addEventListener("click", function() {
   cfvForm.style.display = "block";
   tpForm.style.display = "block";
 });
+
+nadaButton.addEventListener("click", function() {
+  loginForm.style.display = "none";
+  registerForm.style.display = "none";
+  cfvForm.style.display = "none";
+  tpForm.style.display = "none";
+  panelViajeButton.style.display = "none";
+  panelViaje2Button.style.display = "none";
+});
+
