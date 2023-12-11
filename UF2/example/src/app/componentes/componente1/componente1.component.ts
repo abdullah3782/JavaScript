00028e8fbce7413dce92adb69666a43420d66bcd
abdullah3782 <abdullah.waris098@gmail.com>
2023-12-11 11:31:05 +0100

    as

---
+++ UF2/example/src/app/componentes/componente1/componente1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente1',
+  templateUrl: './componente1.component.html',
+  styleUrls: ['./componente1.component.css']
+})
+export class Componente1Component {
+nombre:string="MARIA"
+edad:number=10
+
+}
